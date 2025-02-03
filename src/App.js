8d95d953367c47eb89234276be4fbd831825451b
vaishalikhandoba79 <vaishalikhandoba@gmail.com>
2025-02-03 21:23:16 +0530

    React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+
+
 
 function App() {
+  // let ele = React.createElement("div", {className:"App"},
+  //   React.createElement("h1",null,"Header")
+  // )
+  let username = "Vaishali";
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-   */}
-   <h1 style={{color:"blue"}}>Welcome to React</h1>
+        <h1 style={{color:"blue"}}>Welcome to React</h1>
     </div>
   );
 }
